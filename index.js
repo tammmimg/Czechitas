bf0b09b2d6e19ffe3761715acb9ffdcab84c78b9
tammmimg <mgrigorovova@gmail.com>
2022-02-22 17:05:11 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
+CONSOLE.LOG("AHOJ");
